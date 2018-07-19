9f13f102dcc9abbb978741d9230755df0d392c1c
Andrew Brown <drusepth@gmail.com>
2018-07-19 00:54:05 -0500

    tooltips on cell hovering

--- ui.js
+++ ui.js
@@ -4,12 +4,14 @@ setTimeout(function() {
       canvas_top_offset  = canvas.offsetTop,
       context            = canvas.getContext('2d');
 
-  canvas.addEventListener('click', function(event) {
+  canvas.addEventListener('mousemove', function(event) {
     let x = x_index_from_coordinate(event.pageX - canvas_left_offset),
         y = y_index_from_coordinate(event.pageY - canvas_top_offset);
 
     let clicked_tile = grid[x][y];
-    console.log(clicked_tile.map(entity_id => id_to_entity_class(entity_id).name));
+    let tile_entities = clicked_tile.map(entity_id => id_to_entity_class(entity_id).name);
 
+    // This is a dirty hack that will break if we ever add more tooltips to the page.
+    document.getElementsByClassName('tooltip-inner')[0].innerText = tile_entities;
   }, false);
 }, 1000);
